8802d14cf24787b253603c3a0d413b4d38eedfb4
Matt Platts <mattplatts@gmail.com>
2015-10-30 17:30:28 +0000

    tweakage

--- validate_and_correct.js
+++ validate_and_correct.js
@@ -1,3 +1,4 @@
+
 /*
  * CLASS: validator
    Allows validation (with true/false return) and auto-correction (removing disallowed characters) of strings
@@ -41,7 +42,7 @@ function validator(){
 	}
 
 	/* Auto correct functions - these return corrected input. */
-	validator.prototype.correct = function (input,to){
+	validator.prototype.autocorrect = function (input,to){
 		
 		var _v = this;
 		
@@ -218,116 +219,129 @@ function validator(){
 	 * Function checkCreditCardFormat
 	 *
 	*/
-validator.prototype.validateCreditCard = function(callback) {
-    var card_types, get_card_type, is_valid_length, is_valid_luhn, normalize, validate, validate_number;
-    card_types = [
-      {
-        name: 'amex',
-        pattern: /^3[47]/,
-        valid_length: [15]
-      }, {
-        name: 'diners_club_carte_blanche',
-        pattern: /^30[0-5]/,
-        valid_length: [14]
-      }, {
-        name: 'diners_club_international',
-        pattern: /^36/,
-        valid_length: [14]
-      }, {
-        name: 'jcb',
-        pattern: /^35(2[89]|[3-8][0-9])/,
-        valid_length: [16]
-      }, {
-        name: 'laser',
-        pattern: /^(6304|670[69]|6771)/,
-        valid_length: [16, 17, 18, 19]
-      }, {
-        name: 'visa_electron',
-        pattern: /^(4026|417500|4508|4844|491(3|7))/,
-        valid_length: [16]
-      }, {
-        name: 'visa',
-        pattern: /^4/,
-        valid_length: [16]
-      }, {
-        name: 'mastercard',
-        pattern: /^5[1-5]/,
-        valid_length: [16]
-      }, {
-        name: 'maestro',
-        pattern: /^(50|(5[6-9]|6[0-9])\d\d\d\d[\d]{6,13})/,
-        valid_length: [12, 13, 14, 15, 16, 17, 18, 19]
-      }, {
-        name: 'discover',
-        pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/,
-        valid_length: [16]
-      }
-    ];
-    get_card_type = function(number) {
-      var card_type, _i, _len;
-      for (_i = 0, _len = card_types.length; _i < _len; _i++) {
-        card_type = card_types[_i];
-        if (number.match(card_type.pattern)) {
-          return card_type;
-        }
-      }
-      return null;
-    };
 
-    is_valid_luhn = function(number) {
-      var digit, n, sum, _i, _len, _ref;
-      sum = 0;
-      _ref = number.split('').reverse();
-      for (n = _i = 0, _len = _ref.length; _i < _len; n = ++_i) {
-        digit = _ref[n];
-        digit = +digit;
-        if (n % 2) {
-          digit *= 2;
-          if (digit < 10) {
-            sum += digit;
-          } else {
-            sum += digit - 9;
-          }
-        } else {
-          sum += digit;
-        }
-      }
-      return sum % 10 === 0;
-    };
+	/* Finction __indexOf - index of array */
+	var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-    is_valid_length = function(number, card_type) {
-      var _ref;
-      return _ref = number.length, __indexOf.call(card_type.valid_length, _ref) >= 0;
-    };
+	validator.prototype.validateCreditCard = function(cardNumber) {
+	    var card_types, get_card_type, is_valid_length, is_valid_luhn, normalize, validate, validate_number;
+	    card_types = [
+	      {
+		name: 'amex',
+		pattern: /^3[47]/,
+		valid_length: [15]
+	      }, {
+		name: 'diners_club_carte_blanche',
+		pattern: /^30[0-5]/,
+		valid_length: [14]
+	      }, {
+		name: 'diners_club_international',
+		pattern: /^36/,
+		valid_length: [14]
+	      }, {
+		name: 'jcb',
+		pattern: /^35(2[89]|[3-8][0-9])/,
+		valid_length: [16]
+	      }, {
+		name: 'laser',
+		pattern: /^(6304|670[69]|6771)/,
+		valid_length: [16, 17, 18, 19]
+	      }, {
+		name: 'visa_electron',
+		pattern: /^(4026|417500|4508|4844|491(3|7))/,
+		valid_length: [16]
+	      }, {
+		name: 'visa',
+		pattern: /^4/,
+		valid_length: [16]
+	      }, {
+		name: 'mastercard',
+		pattern: /^5[1-5]/,
+		valid_length: [16]
+	      }, {
+		name: 'maestro',
+		pattern: /^(50|(5[6-9]|6[0-9])\d\d\d\d[\d]{6,13})/,
+		valid_length: [12, 13, 14, 15, 16, 17, 18, 19]
+	      }, {
+		name: 'discover',
+		pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/,
+		valid_length: [16]
+	      }
+	    ];
+	    get_card_type = function(cardNumber) {
+	      var card_type, _i, _len;
+	      for (_i = 0, _len = card_types.length; _i < _len; _i++) {
+		card_type = card_types[_i];
+		if (cardNumber.match(card_type.pattern)) {
+		  return card_type;
+		}
+	      }
+	      return null;
+	    };
 
-    validate_number = function(number) {
-      var card_type, length_valid, luhn_valid;
-      card_type = get_card_type(number);
-      luhn_valid = false;
-      length_valid = false;
-      if (card_type != null) {
-        luhn_valid = is_valid_luhn(number);
-        length_valid = is_valid_length(number, card_type);
-      }
-      if(number==1000380000000004 || number==1000350000000536) return true;
-      if(luhn_valid && length_valid) return true;
-      return false;
-    };
+	    is_valid_luhn = function(cardNumber) {
+	      var digit, n, sum, _i, _len, _ref;
+	      sum = 0;
+	      _ref = cardNumber.split('').reverse();
+	      for (n = _i = 0, _len = _ref.length; _i < _len; n = ++_i) {
+		digit = _ref[n];
+		digit = +digit;
+		if (n % 2) {
+		  digit *= 2;
+		  if (digit < 10) {
+		    sum += digit;
+		  } else {
+		    sum += digit - 9;
+		  }
+		} else {
+		  sum += digit;
+		}
+	      }
+	      return sum % 10 === 0;
+	    };
 
-    creditcard = function() {
-      var number;
-      number = normalize($(this).val());
-      return validate_number(number);
-    };
+	    is_valid_length = function(cardNumber, card_type) {
+	      var _ref;
+	      return _ref = cardNumber.length, __indexOf.call(card_type.valid_length, _ref) >= 0;
+	    };
 
-    normalize = function(number) {
-      return number.replace(/[ -]/g, '');
-    };
+	    validate_number = function(cardNumber) {
+	      var card_type, length_valid, luhn_valid;
+	      card_type = get_card_type(cardNumber);
+		alert(card_type);
+	console.log(card_type);
+	      luhn_valid = false;
+	      length_valid = false;
+	      if (card_type != null) {
+			luhn_valid = is_valid_luhn(cardNumber);
+			length_valid = is_valid_length(cardNumber, card_type);
+	      }
+	      if(cardNumber==1000380000000004 || cardNumber==1000350000000536) return true;
+	      if(luhn_valid && length_valid) return true;
+	      return false;
+	    };
 
-    if (this.length !== 0) {
-      return validate.call(this);
-    }
-  };
+	    creditcard = function(cardNumber) {
+	      return validate_number(cardNumber);
+	    };
+
+	    normalize = function(cardNumber) {
+	      return cardNumber.replace(/[ -]/g, '');
+	    };
+
+	    if (this.length !== 0) {
+	      return creditcard.call(this,cardNumber);
+	    }
+  	};
+
+
+	/* Credit card */
+	validator.prototype.creditcard = function(cardNumber){
+
+		var _v = this;
+		_v.validateCreditCard(cardNumber);
+	}
+
 
 	/*
 	 * Function: futureDate - tests that a date is in the future
@@ -353,7 +367,16 @@ validator.prototype.validateCreditCard = function(callback) {
 
 }
  
+/* Add to string prototype */
+String.prototype._validate = function(input,modifier){
+	var _v = new validator();
+	return _v.validate(input,modifier);	
+}
 
+/* Add to string prototype */
+String.prototype._autocorrect = function(input,modifier){
+	var _v = new validator();
+	return _v.autocorrect(input,modifier);	
+}
 
-
-var _v = new validator();
+var _v = new validator(); // if you don't want to use the string prototype directly, use _v.function(input,type);
